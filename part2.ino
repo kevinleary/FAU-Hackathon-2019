e2741d2d07226418b6398d2386edf43bbffa45cc
Kevin Leary <kevin.n.leary@gmail.com>
2019-02-08 19:39:40 -0500

    Create part2.ino
    used for part 2 of github to combine all components

---
+++ part2.ino
@@ -0,0 +1,96 @@
+setup(){
+
+#include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+
+// --------------------------------------------------------------------------------------------
+//        UPDATE CONFIGURATION TO MATCH YOUR ENVIRONMENT
+// --------------------------------------------------------------------------------------------
+
+// Add GPIO pins used to connect devices
+#define RGB_PIN 5 // GPIO pin the data line of RGB LED is connected to
+#define DHT_PIN 4 // GPIO pin the data line of the DHT sensor is connected to
+
+// Specify DHT11 (Blue) or DHT22 (White) sensor
+#define DHTTYPE DHT11
+#define NEOPIXEL_TYPE NEO_RGB + NEO_KHZ800
+
+// Temperatures to set LED by (assume temp in C)
+#define ALARM_COLD 0.0
+#define ALARM_HOT 30.0
+#define WARN_COLD 10.0
+#define WARN_HOT 25.0
+
+
+// Add WiFi connection information
+char ssid[] = "fau";  // your network SSID (name)
+char pass[] = "";  // your network password
+
+
+// --------------------------------------------------------------------------------------------
+//        SHOULD NOT NEED TO CHANGE ANYTHING BELOW THIS LINE
+// --------------------------------------------------------------------------------------------
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, RGB_PIN, NEOPIXEL_TYPE);
+DHT dht(DHT_PIN, DHTTYPE);
+
+// variables to hold data
+StaticJsonBuffer<100> jsonBuffer;
+JsonObject& payload = jsonBuffer.createObject();
+JsonObject& status = payload.createNestedObject("d");
+static char msg[50];
+float h = 0.0; // humidity
+float t = 0.0; // temperature
+unsigned char r = 0; // LED RED value
+unsigned char g = 0; // LED Green value
+unsigned char b = 0; // LED Blue value
+
+// Start serial console
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+  while (!Serial) { }
+  Serial.println();
+  Serial.println("ESP32S Sensor Application");
+
+  // Start WiFi connection
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+
+  // Start connected devices
+  dht.begin();
+  pixel.begin();
+
+}
+
+void loop()
+{
+  h = dht.readHumidity();
+  t = dht.readTemperature(); // uncomment this line for Celsius
+  // t = dht.readTemperature(true); // uncomment this line for Fahrenheit
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    // Set RGB LED Colour based on temp
+    b = (t < ALARM_COLD) ? 255 : ((t < WARN_COLD) ? 150 : 0);
+    r = (t >= ALARM_HOT) ? 255 : ((t > WARN_HOT) ? 150 : 0);
+    g = (t > ALARM_COLD) ? ((t <= WARN_HOT) ? 255 : ((t < ALARM_HOT) ? 150 : 0)) : 0;
+    pixel.setPixelColor(0, r, g, b);
+    pixel.show();
+
+    // Print Message to console in JSON format
+    status["temp"] = t;
+    status["humidity"] = h;
+    payload.printTo(msg, 50);
+    Serial.println(msg);
+  }
+  delay(10000);
+}
